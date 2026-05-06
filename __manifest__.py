91f63a2541b84ccfc123f7a67e1bd8d8ea556d55
Huan Nguyen <huan.nguyen@intellisyncdata.com>
2026-05-06 22:45:42 +0700

    Revert 19 to 18

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Survey API Helper',
-    'version': '19.0.0.0.0',
+    'version': '18.0.1.0.0',
     'category': 'ISD Modules',
     'summary': 'API tools to help integrate with Survey module',
     'description': """
